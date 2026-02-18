d62bf17dea05eac5e4994afa943196007013b9a6
Cullan Mackrel <cullanmackrel@gmail.com>
2026-02-18 11:42:58 -0700

    Converted times to UTC, formed 6 hour averages and accounted for daylight savings

--- WoodBuffaloDataCleaning.R
+++ WoodBuffaloDataCleaning.R
@@ -4,38 +4,67 @@ WBH2S <- read_csv("WBH2S(Data).csv")
 #Cleaning up data
 WBH2S <- read_csv("WBH2S(Data).csv", skip = 1, n_max = 227954) 
  StdWBH2S <- WBH2S %>%
-  mutate( "Date" = WBH2S$...1) %>%
+  mutate( "date" = WBH2S$...1) %>%
    slice(-1) %>%
  select(-`...1`) %>%
   mutate(across(.cols = starts_with("AMS"), ~ replace(., . == 'Z', 0))) %>%
 select( -c(`...27`, `...28`,`Wood Buffalo Environmental Association - Continuous Ambient Air Quality Monitoring Program`, `...30`, `...31`, `...32`, `...33`, `...34`)) %>%
-   mutate(across(-Date, as.numeric)) %>%
-   select(`Date`, everything()) %>%
-   separate(Date,
-            into = c("Date", "Time"),
+   mutate(across(-date, as.numeric)) %>%
+   select(`date`, everything()) %>%
+   separate(date,
+            into = c("date", "Time"),
             sep = " ") %>%
- mutate(Time = case_when(Time == "01:00" ~ 08:00,
-                         Time == "02:00" ~ 09:00,
-                         Time == "03:00" ~ 10:00,
-                         Time == "04:00" ~ 11:00,
-                         Time == "05:00" ~ 12:00,
-                         Time == "06:00" ~ 13:00,
-                         Time == "07:00" ~ 14:00,
-                         Time == "08:00" ~ 15:00,
-                         Time == "09:00" ~ 16:00,
-                         Time == "10:00" ~ 17:00,
-                         Time == "11:00" ~ 18:00,
-                         Time == "12:00" ~ 19:00,
-                         Time == "13:00" ~ 20:00,
-                         Time == "14:00" ~ 21:00,
-                         Time == "15:00" ~ 22:00,
-                         Time == "16:00" ~ 23:00,
-                         Time == "17:00" ~ 00:00,
-                         Time == "18:00" ~ 01:00,
-                         Time == "19:00" ~ 02:00,
-                         Time == "20:00" ~ 03:00,
-                         Time == "21:00" ~ 04:00,
-                         Time == "22:00" ~ 05:00,
-                         Time == "23:00" ~ 06:00,
-                         Time == "00:00" ~ 07:00))
+   rename(Hour = Time) %>%
+   unite("date", c("date", "Hour"), sep = " " ) %>%
+   arrange(date) %>%
+   mutate(date = paste(as.character(date), StdWBH2S$Hour, sep = " "),
+          date = as.POSIXct(date, format = "%Y-%m-%d %H")) %>%
+   timeAverage(avg.time = "6 hour") %>%
+   separate(col = date, into = c("date", "Hour"),sep = " ")  %>%
+   mutate(Hour = replace_na(Hour, "00:00:00"), 
+          Hour = case_when(Hour == "00:00:00" ~ 00,
+                           Hour == "06:00:00" ~ 06,
+                           Hour == "12:00:00" ~ 12,
+                           Hour == "18:00:00" ~ 18,
+                           Hour == "07:00:00" ~ 06,
+                           Hour == "13:00:00" ~ 12,
+                           Hour == "19:00:00" ~ 18,
+                           Hour == "01:00:00" ~ 00)) %>%
+          
+          mutate(date = force_tzs(date,tzones = "UTC" ))
+ 
+#Plotting
+H2SPlot <- StdWBH2S %>%
+   timePlot(pollutant = "AMS 4",
+            group = TRUE,
+            stack = TRUE,
+            date.pad = TRUE) 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  
